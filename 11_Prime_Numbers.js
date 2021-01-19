b9830ea078e7f78e14187062fe9ffe019d94ac55
Fiter Pandiangan <fiterpan@users.noreply.github.com>
2021-01-19 09:20:30 +0700

    Create 11_Prime_Numbers.js

---
+++ 11_Prime_Numbers.js
@@ -0,0 +1,17 @@
+
+showPrime(10);
+
+function showPrime(limit){
+    for(let i = 2; i <= limit; i++){
+        let isPrime= true;
+    
+        for(let j = 2; j < i; j++){
+            if(i % j === 0){
+                isPrime = false;
+                break;
+            }
+        }
+
+        if(isPrime) console.log(i);
+    }
+}
