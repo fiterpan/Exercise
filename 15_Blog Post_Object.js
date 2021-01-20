9e1925f47ca76f8e5775f06505d58ced235e03f1
Fiter Pandiangan <fiterpan@users.noreply.github.com>
2021-01-20 22:05:13 +0700

    Create 15_Blog Post_Object.js

---
+++ 15_Blog Post_Object.js
@@ -0,0 +1,14 @@
+let post = {
+    title: 'Padadang-Padadang',
+    body: 'Dududung-Dududung',
+    author: 'Dededeng-Dededeng',
+    view: 10,
+    comments: [
+        {author: 'HeHoHi', body:'LaLiLuLeLo'},
+        {author: 'HuHaHu', body:'OaOiOuOeOo'},
+    ],
+    isLive: true
+};
+
+console.log(post);
+
