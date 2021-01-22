37cd5ec6a2b8df90bc01a2ead743700477f80d4c
Fiter Pandiangan <fiterpan@users.noreply.github.com>
2021-01-22 09:01:13 +0700

    Create 18_Array_from_Range.js

---
+++ 18_Array_from_Range.js
@@ -0,0 +1,13 @@
+function arrayFromRange(min,max){
+    let answer = [];
+    for(i = min; i <= max; i++)
+        answer.push(i);
+        return answer
+}
+
+const numbers= arrayFromRange(-10,-4);
+console.log(numbers);
+
+// let myArray = [1,2,3,4,5];
+// myArray.push(7);
+// console.log(myArray);
